b6762a9313ad38b1110ec1615085d491446c86d2
Nidhi <nidhii200107@gmail.com>
2023-09-10 14:46:41 +0530

    first

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+// export const mongoDBURL = 'mongodb+srv://nidhi_project:Root1@bookstore.nhvfb6j.mongodb.net/books-collection?retryWrites=true&w=majority';
+// export const mongoDBURL ='mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+// export const mongoDBURL = 'mongodb+srv://nidhi_project:Root1@bookstore.nhvfb6j.mongodb.net/books?retryWrites=true&w=majority';
+export const mongoDBURL = 'mongodb+srv://abcd123:abcd123@bookstore.nhvfb6j.mongodb.net/?retryWrites=true&w=majority';
